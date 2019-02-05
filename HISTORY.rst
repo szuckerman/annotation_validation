e860c04c776573ac6f3fad1b100c9d3e80433f30
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2019-02-05 15:53:59 -0500

    Add caching to getfullargspec and get_type_hints

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,11 @@
 History
 =======
 
+0.1.1 (2019-02-05)
+------------------
+
+* Adding caching to `getfullargspec` and `get_type_hints`.
+
 0.1.0 (2019-02-04)
 ------------------
 
--- annotation_validation/annotation_validation.py
+++ annotation_validation/annotation_validation.py
@@ -3,20 +3,31 @@
 """Main module."""
 
 
-from functools import wraps
+from functools import wraps, lru_cache
 from inspect import getfullargspec
-import typing
+from typing import get_type_hints, Union
 
 
-def _validate_types(obj, **kwargs):
-    hints = typing.get_type_hints(obj)
+@lru_cache(maxsize=128)
+def getfullargspec_cache(func):
+    return getfullargspec(func)
+
+
+@lru_cache(maxsize=128)
+def get_type_hints_cache(func):
+    hints = get_type_hints(func)
+    return_type_list = [(k, v) for k, v in hints.items() if k == 'return']
+    return hints, return_type_list
+
+
+def _validate_types(hints, **kwargs):
     # iterate all type hints
     for attr_name, attr_type in hints.items():
 
         if attr_name == 'return':
             continue
 
-        if type(attr_type) == type(typing.Union):
+        if type(attr_type) == type(Union):
             if not isinstance(kwargs[attr_name], attr_type.__args__):
                 raise TypeError(
                     '%r is not one of types %s' % (attr_name, attr_type)
@@ -29,30 +40,20 @@ def _validate_types(obj, **kwargs):
                 )
 
 
-def _validate_return(obj, result):
-    hints = typing.get_type_hints(obj)
+def _validate_return(return_type, result):
     # iterate all type hints
     # result_type = type(result)
-    for attr_name, attr_type in hints.items():
+    if type(return_type) == type(Union):
+        if not isinstance(result, return_type.__args__):
+            raise TypeError(
+                'return value is not one of types %s' % (return_type.__args__)
+            )
 
-        if attr_name == 'return':
-
-            if type(attr_type) == type(typing.Union):
-                if not isinstance(result, attr_type.__args__):
-                    raise TypeError(
-                        'return value is not one of types %s' % (attr_type.__args__)
-                    )
-
-            else:
-                if not isinstance(result, attr_type):
-                    raise TypeError(
-                        'return value is not of type %s' % (attr_type)
-                    )
-
-        else:
-            continue
-
-
+    else:
+        if not isinstance(result, return_type):
+            raise TypeError(
+                'return value is not of type %s' % (return_type)
+            )
 
 
 def validate_types(decorator):
@@ -60,15 +61,21 @@ def validate_types(decorator):
     def wrapped_decorator(*args, **kwargs):
 
         # translate *args into **kwargs
-        func_args = getfullargspec(decorator)[0]
+        func_args = getfullargspec_cache(decorator)[0]
         kwargs.update(dict(zip(func_args, args)))
 
-        _validate_types(decorator, **kwargs)
+        hints, return_type_list = get_type_hints_cache(decorator)
+
+        _validate_types(hints, **kwargs)
 
         result=decorator(**kwargs)
 
-        _validate_return(decorator, result)
+        if return_type_list:
+            return_type = return_type_list[0][1]
+            _validate_return(return_type, result)
 
         return result
 
     return wrapped_decorator
+
+
