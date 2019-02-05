01c920a4c1d2952757d7af72302f922a6e440dff
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2019-02-05 16:00:25 -0500

    Update version

--- annotation_validation/__init__.py
+++ annotation_validation/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Sam Zuckerman"""
 __email__ = 'sam.p.zuckerman@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from annotation_validation.annotation_validation import validate_types
